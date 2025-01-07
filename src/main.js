bd67c51f330e6e09e6a4687626593a5949479691
Tiago Faustino <tiagobfaustino@gmail.com>
2025-01-07 00:03:00 -0300

    🚧 Create component PlayerName component without Css

--- src/main.js
+++ src/main.js
@@ -2,15 +2,23 @@ import "./styles/settings/colors.css";
 import "./styles/generic/reset.css";
 import "./styles/elements/base.css";
 import BoardGame from "./components/objects/BoardGame";
+import PlayerName from "./components/PlayerName";
 
 
 
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(6);
 
 
 //// não usar - problemas na segurança
 //$root.innerHTML = $htmlCardGame;
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+$root.insertAdjacentHTML(
+    "beforeend", 
+    `
+        ${PlayerName('Player1')}
+        ${PlayerName('Player2')}
+        ${BoardGame(6)}
+
+    `
+);
