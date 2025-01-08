79eeb8e388a5dc146cd110074ce4db2718e3ced3
Tiago Faustino <tiagobfaustino@gmail.com>
2025-01-08 19:13:12 -0300

    🎨 Add html comment in template string

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 //similar ao JSX do react
 
 function CardGame() {
-    return ` 
+    return /*html*/ ` 
         <article class="card-game">
             <img src="images/alura-pixel 1.png" alt="Logo da Alura" class="icon">
         </article>
