f7222597e13ff76d2234403695b57556fae22a74
Tiago Faustino <tiagobfaustino@gmail.com>
2025-01-09 16:59:13 -0300

    ✨ Add flip CardGame

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,12 +1,18 @@
 import CardGame from "../../components/CardGame";
+import "./style.css";
 
 function CardFrontBack() {
     return /*html*/ `
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame()}
+            <div class="card -front">
+                ${CardGame()}
+            </div>
+            <div class="card -back" ">
+                ${CardGame("javascript", "Logo do JavaScript")}
+            </div>
         </article>
     `;
+
 }
 
 export default CardFrontBack;
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,10 +2,10 @@ import "./style.css";
 
 //similar ao JSX do react
 
-function CardGame() {
+function CardGame(icon="alura-pixel", alt="logo da Alura") {
     return /*html*/ ` 
         <article class="card-game">
-            <img src="images/alura-pixel 1.png" alt="Logo da Alura" class="icon">
+            <img src="images/${icon}.png" alt="${alt}">
         </article>
     `
 }
