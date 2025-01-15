8ee6afdb86351c9460702b708b2377ebd4fe7ee4
Tiago Faustino <tiagobfaustino@gmail.com>
2025-01-14 22:44:43 -0300

    ✨ Add click event on CardFrontBack component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,13 +1,28 @@
 import CardGame from "../../components/CardGame";
 import "./style.css";
 
+
 function CardFrontBack() {
+    window.cardFrontBack = {};
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        //const $cardFrontBack = $origin.parentNode.parentNode.parentNode
+        const $cardFrontBack = $origin.closest(".card-front-back");
+        
+        /*if ($cardFrontBack.classList.contains("-active")) {
+            $cardFrontBack.classList.remove("-active")
+        } else {
+            $cardFrontBack.classList.add("-active")
+        }*/
+        $cardFrontBack.classList.toggle("-active");
+    }
+
     return /*html*/ `
-        <article class="card-front-back">
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
-            <div class="card -back" ">
+            <div class="card -back">
                 ${CardGame("javascript", "Logo do JavaScript")}
             </div>
         </article>
--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ $root.insertAdjacentHTML(
     "beforeend", 
     `
         ${ScoreBoard()}
-        ${BoardGame(2)}
+        ${BoardGame(6)}
 
     `
 );
